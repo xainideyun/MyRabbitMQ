d2aeb9a702fe0d4b7382d1847a5602b8f181aa78
华天晓 <sunxsaljj@163.com>
2020-11-23 18:25:19 +0800

    1

---
+++ Htx.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Htx.RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Htx.RabbitMQ.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Htx.RabbitMQ.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// 默认方法
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object Index([FromQuery]string msg, [FromQuery]string name)
+        {
+            name ??= "admin";
+            msg ??= DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return new { name, msg };
+        }
+    }
+}
