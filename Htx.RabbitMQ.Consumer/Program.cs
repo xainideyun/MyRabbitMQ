5e549b07f442604f19da9aa173dee35b17be1065
华天晓 <sunxsaljj@163.com>
2020-11-24 17:07:01 +0800

    1

--- Htx.RabbitMQ.Consumer/Program.cs
+++ Htx.RabbitMQ.Consumer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
+using System.Threading;
 
 namespace Htx.RabbitMQ.Consumer
 {
@@ -7,8 +10,66 @@ namespace Htx.RabbitMQ.Consumer
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World!");
+            //Consumer();
+            Subscribe();
         }
+
+        
+        /// <summary>
+        /// 消费者
+        /// </summary>
+        static void Consumer()
+        {
+            var host = new { host = "81.68.253.181", username = "admin", password = "000000", queueName = "myqueue", vhost = "my_vhost" };
+            var factory = new ConnectionFactory() { HostName = host.host, UserName = host.username, Password = host.password, VirtualHost = host.vhost };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(host.queueName, true, false, false, null);
+
+            // 设置每次最多可以接收多少条消息，prefetchCount=1时，表明只有消息处理完成且返回确认后方可接收新消息
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+                //Thread.Sleep(3000);
+                channel.BasicAck(ea.DeliveryTag, false);    // 返回消息处理完成确认信息
+            };
+            channel.BasicConsume(
+                queue: host.queueName,          // 队列名称
+                autoAck: false,                 // 是否自动确认
+                consumer: consumer              // 消费者
+            );
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// 订阅者
+        /// </summary>
+        static void Subscribe()
+        {
+            var host = new { host = "81.68.253.181", username = "admin", password = "000000", queueName = "myqueue", vhost = "my_vhost" };
+            var factory = new ConnectionFactory() { HostName = host.host, UserName = host.username, Password = host.password, VirtualHost = host.vhost };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);           // 定义路由
+            var queueName = channel.QueueDeclare().QueueName;               // 定义队列名称
+            channel.QueueBind(queueName, "logs", "");                       // 绑定路由
+
+            var subscribe = new EventingBasicConsumer(channel);
+            subscribe.Received += (model, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume(queueName, true, subscribe);
+            Console.ReadLine();
+        }
+
     }
 }
--- Htx.RabbitMQ.WebApp/Controllers/HomeController.cs
+++ Htx.RabbitMQ.WebApp/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace Htx.RabbitMQ.WebApp.Controllers
     public class HomeController : ControllerBase
     {
         /// <summary>
-        /// 默认方法
+        /// 生产消费
         /// </summary>
         /// <param name="msg"></param>
         /// <param name="name"></param>
@@ -28,17 +28,18 @@ namespace Htx.RabbitMQ.WebApp.Controllers
             return new { name, msg };
         }
 
-
         private void Send(string msg, string name)
         {
-            var host = new { host = "81.68.253.181", username = "admin", password = "000000", queueName = "myqueue" };
-            var factory = new ConnectionFactory() { HostName = host.host, UserName = host.username, Password = host.password, VirtualHost = "my_vhost" };
+            var host = new { host = "81.68.253.181", username = "admin", password = "000000", queueName = "myqueue", vhost = "my_vhost" };
+            var factory = new ConnectionFactory { HostName = host.host, UserName = host.username, Password = host.password, VirtualHost = host.vhost };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;           // 设置消息持久性
             channel.QueueDeclare(
                     queue: host.queueName,
-                    durable: false,
+                    durable: true,                  // 设置队列持久性
                     exclusive: false,
                     autoDelete: false,
                     arguments: null
@@ -46,10 +47,33 @@ namespace Htx.RabbitMQ.WebApp.Controllers
             channel.BasicPublish(
                     exchange: "",
                     routingKey: host.queueName,
-                    basicProperties: null,
+                    basicProperties: properties,    // 设置消息持久性
                     body: GetBuffer($"用户{name}发送消息：{msg}")
             );
 
+        }
+
+        /// <summary>
+        /// 发布订阅
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("publish")]
+        public object Publish([FromQuery] string msg, [FromQuery] string name)
+        {
+            var host = new { host = "81.68.253.181", username = "admin", password = "000000", queueName = "myqueue", vhost = "my_vhost" };
+            var factory = new ConnectionFactory { HostName = host.host, UserName = host.username, Password = host.password, VirtualHost = host.vhost };
+            using var conn = factory.CreateConnection();
+            using var channel = conn.CreateModel();
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+            channel.BasicPublish(
+                exchange: "logs",
+                routingKey: "",
+                basicProperties: null,
+                body: GetBuffer($"用户{name}发送消息：{msg}")
+            );
+            return new { msg, name };
         }
 
         private byte[] GetBuffer(string msg)
